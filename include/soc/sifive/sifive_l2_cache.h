22fbee13ddfca3f130654af43fdc643d0e252f31
Tom <support@vamrs.com>
2021-01-08 02:54:51 +0800

    sifive/sifive_l2_cache: Add sifive_l2_flush64_range function

--- include/soc/sifive/sifive_l2_cache.h
+++ include/soc/sifive/sifive_l2_cache.h
@@ -7,6 +7,10 @@
 #ifndef __SOC_SIFIVE_L2_CACHE_H
 #define __SOC_SIFIVE_L2_CACHE_H
 
+#ifdef CONFIG_SIFIVE_L2_FLUSH
+extern void sifive_l2_flush64_range(unsigned long start, unsigned long len);
+#endif
+
 extern int register_sifive_l2_error_notifier(struct notifier_block *nb);
 extern int unregister_sifive_l2_error_notifier(struct notifier_block *nb);
 
