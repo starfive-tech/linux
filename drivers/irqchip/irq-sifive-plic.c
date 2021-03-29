f6fbd5da1399af9198dfca95447989480431ca2e
Jisheng Zhang <jszhang@kernel.org>
2021-03-30 02:09:11 +0800

    irqchip/sifive-plic: Mark two global variables __ro_after_init
    All of these two are never modified after init, so they can be
     __ro_after_init.
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20210330020911.26423e9e@xhacker

--- drivers/irqchip/irq-sifive-plic.c
+++ drivers/irqchip/irq-sifive-plic.c
@@ -82,8 +82,8 @@ struct plic_handler {
 	/* To record interrupts that are enabled before suspend. */
 	u32 enable_reg[MAX_DEVICES / 32];
 };
-static int plic_parent_irq;
-static bool plic_cpuhp_setup_done;
+static int plic_parent_irq __ro_after_init;
+static bool plic_cpuhp_setup_done __ro_after_init;
 static DEFINE_PER_CPU(struct plic_handler, plic_handlers);
 static struct plic_priv *priv_data;
 
