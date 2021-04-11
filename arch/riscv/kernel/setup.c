73202cf7f3c92c6eaa46c382411b78f92635d481
Alexandre Ghiti <alex@ghiti.fr>
2021-04-11 12:41:44 -0400

    riscv: Move kernel mapping outside of linear mapping
    This is a preparatory patch for relocatable kernel and sv48 support.
    
    The kernel used to be linked at PAGE_OFFSET address therefore we could use
    the linear mapping for the kernel mapping. But the relocated kernel base
    address will be different from PAGE_OFFSET and since in the linear mapping,
    two different virtual addresses cannot point to the same physical address,
    the kernel mapping needs to lie outside the linear mapping so that we don't
    have to copy it at the same physical offset.
    
    The kernel mapping is moved to the last 2GB of the address space, BPF
    is now always after the kernel and modules use the 2GB memory range right
    before the kernel, so BPF and modules regions do not overlap. KASLR
    implementation will simply have to move the kernel in the last 2GB range
    and just take care of leaving enough space for BPF.
    
    In addition, by moving the kernel to the end of the address space, both
    sv39 and sv48 kernels will be exactly the same without needing to be
    relocated at runtime.
    
    Reference from: 2bfc6cd81bd17e4306e24ee47b9554c967bcb499
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>
    [Palmer: Squash the STRICT_RWX fix, and a !MMU fix]
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Signed-off-by: Joshua Yeong <joshua.yeong@starfivetech.com>

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -258,8 +258,13 @@ void __init setup_arch(char **cmdline_p)
 	if (IS_ENABLED(CONFIG_RISCV_SBI))
 		sbi_init();
 
-	if (IS_ENABLED(CONFIG_STRICT_KERNEL_RWX))
+	if (IS_ENABLED(CONFIG_STRICT_KERNEL_RWX)) {
 		protect_kernel_text_data();
+#if defined(CONFIG_64BIT) && defined(CONFIG_MMU)
+		protect_kernel_linear_mapping_text_rodata();
+#endif
+	}
+
 #ifdef CONFIG_SWIOTLB
 	swiotlb_init(1);
 #endif
