cba4458c49a152c573a362ad7e43278a399db60e
Matteo Croce <mcroce@microsoft.com>
2021-05-21 03:26:38 +0200

    net: stmmac: use GFP_DMA32
    Signed-off-by: Matteo Croce <mcroce@microsoft.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1429,14 +1429,16 @@ static int stmmac_init_rx_buffers(struct stmmac_priv *priv, struct dma_desc *p,
 	struct stmmac_rx_buffer *buf = &rx_q->buf_pool[i];
 
 	if (!buf->page) {
-		buf->page = page_pool_dev_alloc_pages(rx_q->page_pool);
+		buf->page = page_pool_alloc_pages(rx_q->page_pool,
+						  GFP_ATOMIC | __GFP_NOWARN | GFP_DMA32);
 		if (!buf->page)
 			return -ENOMEM;
 		buf->page_offset = stmmac_rx_offset(priv);
 	}
 
 	if (priv->sph && !buf->sec_page) {
-		buf->sec_page = page_pool_dev_alloc_pages(rx_q->page_pool);
+		buf->sec_page = page_pool_alloc_pages(rx_q->page_pool,
+						      GFP_ATOMIC | __GFP_NOWARN | GFP_DMA32);
 		if (!buf->sec_page)
 			return -ENOMEM;
 
@@ -4454,13 +4456,15 @@ static inline void stmmac_rx_refill(struct stmmac_priv *priv, u32 queue)
 			p = rx_q->dma_rx + entry;
 
 		if (!buf->page) {
-			buf->page = page_pool_dev_alloc_pages(rx_q->page_pool);
+			buf->page = page_pool_alloc_pages(rx_q->page_pool,
+							  GFP_ATOMIC | __GFP_NOWARN | GFP_DMA32);
 			if (!buf->page)
 				break;
 		}
 
 		if (priv->sph && !buf->sec_page) {
-			buf->sec_page = page_pool_dev_alloc_pages(rx_q->page_pool);
+			buf->sec_page = page_pool_alloc_pages(rx_q->page_pool,
+							      GFP_ATOMIC | __GFP_NOWARN | GFP_DMA32);
 			if (!buf->sec_page)
 				break;
 
