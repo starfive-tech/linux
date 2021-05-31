6597905f2ab9fcff922f185ebbfb59a7364b5c19
Drew Fustini <drew@beagleboard.org>
2021-05-30 23:34:02 -0700

    gpio: starfive-jh7100: add SPDX-License-Identifier
    Add SPDX-License-Identifier and reformat copyright information
    
    Signed-off-by: Drew Fustini <drew@beagleboard.org>

--- drivers/gpio/gpio-starfive-jh7100.c
+++ drivers/gpio/gpio-starfive-jh7100.c
@@ -1,21 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
- ******************************************************************************
- * @file  gpio-starfive-vic.c
- * @author  StarFive Technology
- * @version  V1.0
- * @date  08/13/2020
- * @brief
- ******************************************************************************
- * @copy
+ * GPIO driver for StarFive JH7100 SoC
  *
- * THE PRESENT SOFTWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
- * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
- * TIME. AS A RESULT, STARFIVE SHALL NOT BE HELD LIABLE FOR ANY
- * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
- * FROM THE CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
- * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
- *
- * COPYRIGHT 2020 Shanghai StarFive Technology Co., Ltd.
+ * Copyright (C) 2020 Shanghai StarFive Technology Co., Ltd.
  */
 
 #include <linux/module.h>
