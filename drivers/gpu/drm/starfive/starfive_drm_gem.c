3da8d33cc93b434f1cc356c391095d458ca82a04
andy.hu <andy.hu@starfive.com>
2021-11-23 16:56:30 +0800

    drm: Fix the crash issue when user space drm application use drmPrimeFDToHandle method to import dmabuf from other module
    Signed-off-by: Andy Hu <andy.hu@starfivetech.com>

--- drivers/gpu/drm/starfive/starfive_drm_gem.c
+++ drivers/gpu/drm/starfive/starfive_drm_gem.c
@@ -106,6 +106,7 @@ starfive_drm_gem_alloc_object(struct drm_device *drm, unsigned int size)
 	if (!starfive_obj)
 		return ERR_PTR(-ENOMEM);
 
+	starfive_obj->base.funcs = &starfive_gem_object_funcs;
 	obj = &starfive_obj->base;
 	ret = drm_gem_object_init(drm, obj, round_up(size, PAGE_SIZE));
 	if (ret)
@@ -164,8 +165,6 @@ starfive_drm_gem_create_object(struct drm_device *drm, unsigned int size,
 	ret = starfive_drm_gem_alloc_buf(starfive_obj, alloc_kmap);
 	if (ret)
 		goto err_free_obj;
-
-	starfive_obj->base.funcs = &starfive_gem_object_funcs;
 
 	return starfive_obj;
 
