8bc87c189f86bfb9458bdd7d1cb41b87b81af022
minda.chen <minda.chen@starfivetech.com>
2022-06-23 15:41:23 +0800

    sound:tdm: add tdm audio interface driver support
    Signed-off-by: minda.chen <minda.chen@starfivetech.com>

---
+++ sound/soc/starfive/starfive_tdm.c
@@ -0,0 +1,592 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * TDM driver for the StarFive JH7110 SoC
+ *
+ * Copyright (C) 2021 StarFive Technology Co., Ltd.
+ */
+#include <linux/clk.h>
+#include <linux/device.h>
+#include <linux/reset.h>
+#include <linux/module.h>
+#include <linux/of_irq.h>
+#include <linux/of_platform.h>
+#include <linux/regmap.h>
+#include <sound/soc.h>
+#include <sound/soc-dai.h>
+#include <sound/pcm_params.h>
+#include <sound/initval.h>
+#include <sound/tlv.h>
+#include "starfive_tdm.h"
+
+#define CLOCK_BASE	0x13020000UL
+
+static inline u32 sf_tdm_readl(struct sf_tdm_dev *dev, u16 reg)
+{
+	return readl_relaxed(dev->tdm_base + reg);
+}
+
+static inline void sf_tdm_writel(struct sf_tdm_dev *dev, u16 reg, u32 val)
+{
+	writel_relaxed(val, dev->tdm_base + reg);
+}
+
+static void sf_tdm_start(struct sf_tdm_dev *dev, struct snd_pcm_substream *substream)
+{
+	u32 data;
+	unsigned int val;
+
+	data = sf_tdm_readl(dev, TDM_PCMGBCR);
+	sf_tdm_writel(dev, TDM_PCMGBCR, data | PCMGBCR_ENABLE);
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		val = sf_tdm_readl(dev, TDM_PCMTXCR);
+		sf_tdm_writel(dev, TDM_PCMTXCR, val | PCMTXCR_TXEN);
+	} else {
+		val = sf_tdm_readl(dev, TDM_PCMRXCR);
+		sf_tdm_writel(dev, TDM_PCMRXCR, val | PCMRXCR_RXEN);
+	}
+}
+
+static void sf_tdm_stop(struct sf_tdm_dev *dev, struct snd_pcm_substream *substream)
+{
+	unsigned int val;
+	unsigned int bcr;
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		val = sf_tdm_readl(dev, TDM_PCMTXCR);
+		val &= ~PCMTXCR_TXEN;
+		sf_tdm_writel(dev, TDM_PCMTXCR, val);
+	} else {
+		val = sf_tdm_readl(dev, TDM_PCMRXCR);
+		val &= ~PCMRXCR_RXEN;
+		sf_tdm_writel(dev, TDM_PCMRXCR, val);
+	}
+
+	bcr = sf_tdm_readl(dev, TDM_PCMGBCR);
+	sf_tdm_writel(dev, TDM_PCMGBCR, bcr & PCMGBCR_MASK);
+}
+
+static int sf_tdm_syncdiv(struct sf_tdm_dev *dev)
+{
+	u32 sl, sscale, syncdiv;
+
+	sl = (dev->rx.sl >= dev->tx.sl) ? dev->rx.sl:dev->tx.sl;
+	sscale = (dev->rx.sscale >= dev->tx.sscale) ? dev->rx.sscale:dev->tx.sscale;
+	syncdiv = dev->pcmclk / dev->samplerate - 1;
+
+	if ((syncdiv + 1) < (sl * sscale)) {
+		pr_info("set syncdiv failed !\n");
+		return -1;
+	}
+
+	if ((dev->syncm == TDM_SYNCM_LONG) &&
+			((dev->rx.sscale <= 1) || (dev->tx.sscale <= 1))) {
+		if ((syncdiv + 1) <= sl) {
+			pr_info("set syncdiv failed! it must be (syncdiv+1) > max[tx.sl, rx.sl]\n");
+			return -1;
+		}
+	}
+
+	sf_tdm_writel(dev, TDM_PCMDIV, syncdiv);
+	return 0;
+}
+
+static void sf_tdm_contrl(struct sf_tdm_dev *dev)
+{
+	u32 data;
+
+	data = (dev->clkpolity << 5) | (dev->elm << 3) | (dev->syncm << 2) | (dev->ms_mode << 1);
+	sf_tdm_writel(dev, TDM_PCMGBCR, data);
+}
+
+static void sf_tdm_config(struct sf_tdm_dev *dev, struct snd_pcm_substream *substream)
+{
+	u32 datarx, datatx;
+
+	sf_tdm_stop(dev, substream);
+	sf_tdm_contrl(dev);
+	sf_tdm_syncdiv(dev);
+
+	datarx = (dev->rx.ifl << 11) | (dev->rx.wl << 8) | (dev->rx.sscale << 4) |
+		(dev->rx.sl << 2) | (dev->rx.lrj << 1);
+
+	datatx = (dev->tx.ifl << 11) | (dev->tx.wl << 8) | (dev->tx.sscale << 4) |
+		(dev->tx.sl << 2) | (dev->tx.lrj << 1);
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
+		sf_tdm_writel(dev, TDM_PCMTXCR, datatx);
+	else
+		sf_tdm_writel(dev, TDM_PCMRXCR, datarx);
+}
+
+
+#define sf_tdm_suspend	NULL
+#define sf_tdm_resume	NULL
+
+static const struct snd_soc_component_driver sf_tdm_component = {
+	.name		= "sf-tdm",
+	.suspend	= sf_tdm_suspend,
+	.resume		= sf_tdm_resume,
+};
+
+static int sf_tdm_hw_params(struct snd_pcm_substream *substream,
+		struct snd_pcm_hw_params *params, struct snd_soc_dai *dai)
+{
+	struct sf_tdm_dev *dev = snd_soc_dai_get_drvdata(dai);
+	int chan_wl, chan_sl, chan_nr;
+	struct snd_dmaengine_dai_dma_data *dma_data = NULL;
+	unsigned int data_width;
+	unsigned int mclk_rate;
+	int ret;
+
+	dev->samplerate = params_rate(params);
+	switch (dev->samplerate) {
+	case 8000:
+		mclk_rate = 12288000;
+		dev->pcmclk = 256000;
+		break;
+	case 16000:
+		mclk_rate = 12288000;
+		dev->pcmclk = 512000;
+		break;
+	default:
+		pr_err("TDM: not support sample rate:%d\n", dev->samplerate);
+		return -EINVAL;
+	}
+
+	data_width = params_width(params);
+	dev->pcmclk = 2 * dev->samplerate * data_width;
+
+	switch (params_format(params)) {
+	case SNDRV_PCM_FORMAT_S8:
+		chan_wl = TDM_8BIT_WORD_LEN;
+		chan_sl = TDM_8BIT_SLOT_LEN;
+		break;
+
+	case SNDRV_PCM_FORMAT_S16_LE:
+		chan_wl = TDM_16BIT_WORD_LEN;
+		chan_sl = TDM_16BIT_SLOT_LEN;
+		break;
+
+	case SNDRV_PCM_FORMAT_S24_LE:
+		chan_wl = TDM_24BIT_WORD_LEN;
+		chan_sl = TDM_32BIT_SLOT_LEN;
+		break;
+
+	case SNDRV_PCM_FORMAT_S32_LE:
+		chan_wl = TDM_32BIT_WORD_LEN;
+		chan_sl = TDM_32BIT_SLOT_LEN;
+		break;
+
+	default:
+		dev_err(dev->dev, "tdm: unsupported PCM fmt");
+		return -EINVAL;
+	}
+
+	chan_nr = params_channels(params);
+	switch (chan_nr) {
+	case TWO_CHANNEL_SUPPORT:
+	case FOUR_CHANNEL_SUPPORT:
+	case SIX_CHANNEL_SUPPORT:
+	case EIGHT_CHANNEL_SUPPORT:
+		break;
+	default:
+		dev_err(dev->dev, "channel not supported\n");
+		return -EINVAL;
+	}
+
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
+		dev->tx.wl = chan_wl;
+		dev->tx.sl = chan_sl;
+		dev->tx.sscale = chan_nr;
+		dma_data = &dev->play_dma_data;
+	} else {
+		dev->rx.wl = chan_wl;
+		dev->rx.sl = chan_sl;
+		dev->rx.sscale = chan_nr;
+		dma_data = &dev->capture_dma_data;
+	}
+
+	snd_soc_dai_set_dma_data(dai, substream, dma_data);
+
+	ret = clk_set_rate(dev->clk_mclk_inner, mclk_rate);
+	if (ret) {
+		dev_info(dev->dev, "Can't set clk_mclk: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_set_rate(dev->clk_tdm_internal, dev->pcmclk);
+	if (ret) {
+		dev_info(dev->dev, "Can't set clk_tdm_internal: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_set_parent(dev->clk_tdm, dev->clk_tdm_ext);
+	if (ret) {
+		dev_info(dev->dev, "Can't set clock source for clk_tdm: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_ahb);
+	if (ret) {
+		dev_err(dev->dev, "Failed to prepare enable clk_tdm_ahb\n");
+		return ret;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_apb);
+	if (ret) {
+		dev_err(dev->dev, "Failed to prepare enable clk_tdm_apb\n");
+		return ret;
+	}
+
+	sf_tdm_config(dev, substream);
+
+	return 0;
+}
+
+static int sf_tdm_trigger(struct snd_pcm_substream *substream,
+		int cmd, struct snd_soc_dai *dai)
+{
+	struct sf_tdm_dev *dev = snd_soc_dai_get_drvdata(dai);
+	int ret = 0;
+
+	switch (cmd) {
+	case SNDRV_PCM_TRIGGER_START:
+	case SNDRV_PCM_TRIGGER_RESUME:
+	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
+		dev->active++;
+		sf_tdm_start(dev, substream);
+		break;
+
+	case SNDRV_PCM_TRIGGER_STOP:
+	case SNDRV_PCM_TRIGGER_SUSPEND:
+	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
+		dev->active--;
+		sf_tdm_stop(dev, substream);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static int sf_tdm_set_fmt(struct snd_soc_dai *cpu_dai, unsigned int fmt)
+{
+	struct sf_tdm_dev *dev = snd_soc_dai_get_drvdata(cpu_dai);
+	int ret = 0;
+
+	switch (fmt & SND_SOC_DAIFMT_MASTER_MASK) {
+	case SND_SOC_DAIFMT_CBM_CFM:
+		dev->ms_mode = TDM_AS_SLAVE;
+		break;
+	case SND_SOC_DAIFMT_CBS_CFS:
+		dev->ms_mode = TDM_AS_MASTER;
+		break;
+	case SND_SOC_DAIFMT_CBM_CFS:
+	case SND_SOC_DAIFMT_CBS_CFM:
+		ret = -EINVAL;
+		break;
+	default:
+		dev_dbg(dev->dev, "tdm : Invalid master/slave format\n");
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static const struct snd_soc_dai_ops sf_tdm_dai_ops = {
+	.hw_params	= sf_tdm_hw_params,
+	.trigger	= sf_tdm_trigger,
+	.set_fmt	= sf_tdm_set_fmt,
+};
+
+static int sf_tdm_dai_probe(struct snd_soc_dai *dai)
+{
+	struct sf_tdm_dev *dev = snd_soc_dai_get_drvdata(dai);
+
+	snd_soc_dai_init_dma_data(dai, &dev->play_dma_data, &dev->capture_dma_data);
+	snd_soc_dai_set_drvdata(dai, dev);
+	return 0;
+}
+
+#define SF_TDM_RATE	(SNDRV_PCM_RATE_8000 | \
+			SNDRV_PCM_RATE_16000 | \
+			SNDRV_PCM_RATE_32000)
+
+#define SF_TDM_FORMATS	(SNDRV_PCM_FMTBIT_S8 | \
+			SNDRV_PCM_FMTBIT_S16_LE | \
+			SNDRV_PCM_FMTBIT_S24_LE | \
+			SNDRV_PCM_FMTBIT_S32_LE)
+
+static struct snd_soc_dai_driver sf_tdm_dai = {
+	.name = "sf_tdm",
+	.id = 0,
+	.playback = {
+		.stream_name    = "Playback",
+		.channels_min   = 2,
+		.channels_max   = 8,
+		.rates          = SF_TDM_RATE,
+		.formats        = SF_TDM_FORMATS,
+	},
+	.capture = {
+		.stream_name    = "Capture",
+		.channels_min   = 2,
+		.channels_max   = 8,
+		.rates          = SF_TDM_RATE,
+		.formats        = SF_TDM_FORMATS,
+	},
+	.ops = &sf_tdm_dai_ops,
+	.probe = sf_tdm_dai_probe,
+	.symmetric_rates = 1,
+};
+
+static void tdm_init_params(struct sf_tdm_dev *dev)
+{
+	dev->clkpolity = TDM_TX_RASING_RX_FALLING;
+	if (dev->frame_mode == SHORT_LATER) {
+		dev->elm = TDM_ELM_LATE;
+		dev->syncm = TDM_SYNCM_SHORT;
+	} else if (dev->frame_mode == SHORT_EARLY) {
+		dev->elm = TDM_ELM_EARLY;
+		dev->syncm = TDM_SYNCM_SHORT;
+	} else {
+		dev->elm = TDM_ELM_EARLY;
+		dev->syncm = TDM_SYNCM_LONG;
+	}
+
+	dev->ms_mode = TDM_AS_SLAVE;
+	dev->rx.ifl = dev->tx.ifl = TDM_FIFO_HALF;
+	dev->rx.wl = dev->tx.wl = TDM_16BIT_WORD_LEN;
+	dev->rx.sscale = dev->tx.sscale = 2;
+	dev->rx.lrj = dev->tx.lrj = TDM_LEFT_JUSTIFT;
+	dev->samplerate = 16000;
+	dev->pcmclk = 512000;
+
+	dev->play_dma_data.addr = TDM_FIFO;
+	dev->play_dma_data.addr_width = DMA_SLAVE_BUSWIDTH_2_BYTES;
+	dev->play_dma_data.fifo_size = TDM_FIFO_DEPTH/2;
+	dev->play_dma_data.maxburst = 16;
+
+	dev->capture_dma_data.addr = TDM_FIFO;
+	dev->capture_dma_data.addr_width = DMA_SLAVE_BUSWIDTH_2_BYTES;
+	dev->capture_dma_data.fifo_size = TDM_FIFO_DEPTH/2;
+	dev->capture_dma_data.maxburst = 8;
+}
+
+static int sf_tdm_clk_reset_init(struct platform_device *pdev, struct sf_tdm_dev *dev)
+{
+	int ret;
+
+	static struct clk_bulk_data clks[] = {
+		{ .id = "clk_ahb0" },
+		{ .id = "clk_tdm_ahb" },
+		{ .id = "clk_apb0" },
+		{ .id = "clk_tdm_apb" },
+		{ .id = "clk_tdm_internal" },
+		{ .id = "clk_tdm_ext" },
+		{ .id = "clk_tdm" },
+		{ .id = "mclk_inner" },
+	};
+
+	ret = devm_clk_bulk_get(&pdev->dev, ARRAY_SIZE(clks), clks);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to get tdm clocks\n");
+		goto exit;
+	}
+
+	dev->clk_ahb0 = clks[0].clk;
+	dev->clk_tdm_ahb = clks[1].clk;
+	dev->clk_apb0 = clks[2].clk;
+	dev->clk_tdm_apb = clks[3].clk;
+	dev->clk_tdm_internal = clks[4].clk;
+	dev->clk_tdm_ext = clks[5].clk;
+	dev->clk_tdm = clks[6].clk;
+	dev->clk_mclk_inner = clks[7].clk;
+
+	dev->rst_ahb = devm_reset_control_get_exclusive(&pdev->dev, "tdm_ahb");
+	if (IS_ERR(dev->rst_ahb)) {
+		dev_err(&pdev->dev, "Failed to get tdm_ahb reset control\n");
+		ret = PTR_ERR(dev->rst_ahb);
+		goto exit;
+	}
+
+	dev->rst_apb = devm_reset_control_get_exclusive(&pdev->dev, "tdm_apb");
+	if (IS_ERR(dev->rst_apb)) {
+		dev_err(&pdev->dev, "Failed to get tdm_apb reset control\n");
+		ret = PTR_ERR(dev->rst_apb);
+		goto exit;
+	}
+
+	dev->rst_tdm = devm_reset_control_get_exclusive(&pdev->dev, "tdm_rst");
+	if (IS_ERR(dev->rst_tdm)) {
+		dev_err(&pdev->dev, "Failed to get tdm_rst reset control\n");
+		ret = PTR_ERR(dev->rst_tdm);
+		goto exit;
+	}
+
+	ret = reset_control_assert(dev->rst_ahb);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to assert rst_ahb\n");
+		goto exit;
+	}
+
+	ret = reset_control_assert(dev->rst_apb);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to assert rst_apb\n");
+		goto exit;
+	}
+
+	ret = reset_control_assert(dev->rst_tdm);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to assert rst_tdm\n");
+		goto exit;
+	}
+
+	ret = clk_prepare_enable(dev->clk_mclk_inner);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to prepare enable clk_mclk_inner\n");
+		goto exit;
+	}
+
+	ret = clk_prepare_enable(dev->clk_ahb0);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to prepare enable clk_ahb0\n");
+		goto err_dis_ahb0;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_ahb);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to prepare enable clk_tdm_ahb\n");
+		goto err_dis_tdm_ahb;
+	}
+
+	ret = clk_prepare_enable(dev->clk_apb0);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to prepare enable clk_apb0\n");
+		goto err_dis_apb0;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_apb);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to prepare enable clk_tdm_apb\n");
+		goto err_dis_tdm_apb;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_internal);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to prepare enable clk_tdm_intl\n");
+		goto err_dis_tdm_internal;
+	}
+
+	ret = clk_prepare_enable(dev->clk_tdm_ext);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to prepare enable clk_tdm_ext\n");
+		goto err_dis_tdm_ext;
+	}
+
+	ret = reset_control_deassert(dev->rst_ahb);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to deassert rst_ahb\n");
+		goto err_clk_disable;
+	}
+
+	ret = reset_control_deassert(dev->rst_apb);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to deassert rst_apb\n");
+		goto err_clk_disable;
+	}
+
+	ret = reset_control_deassert(dev->rst_tdm);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to deassert rst_tdm\n");
+		goto err_clk_disable;
+	}
+
+	return 0;
+
+err_clk_disable:
+	clk_disable_unprepare(dev->clk_tdm_ext);
+err_dis_tdm_ext:
+	clk_disable_unprepare(dev->clk_tdm_internal);
+err_dis_tdm_internal:
+	clk_disable_unprepare(dev->clk_tdm_apb);
+err_dis_tdm_apb:
+	clk_disable_unprepare(dev->clk_apb0);
+err_dis_apb0:
+	clk_disable_unprepare(dev->clk_tdm_ahb);
+err_dis_tdm_ahb:
+	clk_disable_unprepare(dev->clk_ahb0);
+err_dis_ahb0:
+	clk_disable_unprepare(dev->clk_mclk_inner);
+exit:
+	return ret;
+}
+
+static int sf_tdm_probe(struct platform_device *pdev)
+{
+	struct sf_tdm_dev *dev;
+	struct resource *res;
+	int ret;
+
+	dev = devm_kzalloc(&pdev->dev, sizeof(*dev), GFP_KERNEL);
+	if (!dev)
+		return -ENOMEM;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	dev->tdm_base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(dev->tdm_base))
+		return PTR_ERR(dev->tdm_base);
+
+	dev->dev = &pdev->dev;
+
+	ret = sf_tdm_clk_reset_init(pdev, dev);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to enable audio-tdm clock\n");
+		return ret;
+	}
+
+	dev->frame_mode = SHORT_LATER;
+	tdm_init_params(dev);
+
+	dev_set_drvdata(&pdev->dev, dev);
+	ret = devm_snd_soc_register_component(&pdev->dev, &sf_tdm_component,
+					 &sf_tdm_dai, 1);
+	if (ret != 0) {
+		dev_err(&pdev->dev, "failed to register dai\n");
+		return ret;
+	}
+
+	ret = devm_snd_dmaengine_pcm_register(&pdev->dev, NULL,	0);
+	if (ret) {
+		dev_err(&pdev->dev, "could not register pcm: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int sf_tdm_dev_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+static const struct of_device_id sf_tdm_of_match[] = {
+	{.compatible = "starfive,sf-tdm",},
+	{}
+};
+MODULE_DEVICE_TABLE(of, sf_tdm_of_match);
+
+static struct platform_driver sf_tdm_driver = {
+
+	.driver = {
+		.name = "sf-tdm",
+		.of_match_table = sf_tdm_of_match,
+	},
+	.probe = sf_tdm_probe,
+	.remove = sf_tdm_dev_remove,
+};
+module_platform_driver(sf_tdm_driver);
+
+MODULE_AUTHOR("Walker Chen <walker.chen@starfivetech.com>");
+MODULE_DESCRIPTION("starfive TDM Controller Driver");
+MODULE_LICENSE("GPL v2");
