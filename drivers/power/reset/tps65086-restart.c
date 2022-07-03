550583357a0df3450e9752135fee86872243b243
Emil Renner Berthing <kernel@esmil.dk>
2022-07-03 21:01:11 +0200

    power: reset: tps65086: Allow building as a module
    Signed-off-by: Emil Renner Berthing <kernel@esmil.dk>

--- drivers/power/reset/tps65086-restart.c
+++ drivers/power/reset/tps65086-restart.c
@@ -95,3 +95,4 @@ module_platform_driver(tps65086_restart_driver);
 
 MODULE_AUTHOR("Emil Renner Berthing <kernel@esmil.dk>");
 MODULE_DESCRIPTION("TPS65086 restart driver");
+MODULE_LICENSE("GPL v2");
