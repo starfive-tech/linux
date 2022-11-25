bc6f67fe5309e8032f0c03ad7c494de6da2f5334
Ley Foon Tan <leyfoon.tan@starfivetech.com>
2022-11-25 15:21:06 +0800

    riscv: starfive: Add DMA non-coherent support
    Enable arch_*dma* implementation.
    
    Signed-off-by: Ley Foon Tan <leyfoon.tan@linux.starfivetech.com>

--- arch/riscv/mm/dma-noncoherent.c
+++ arch/riscv/mm/dma-noncoherent.c
@@ -7,12 +7,66 @@
 
 #include <linux/dma-direct.h>
 #include <linux/dma-map-ops.h>
+#include <linux/io.h>
 #include <linux/mm.h>
 #include <asm/cacheflush.h>
+#include <asm/sbi.h>
 #include <soc/sifive/sifive_ccache.h>
 
 static bool noncoherent_supported;
 
+#ifdef CONFIG_SOC_STARFIVE_DUBHE
+
+#define DUBHE_UNCACHED_OFFSET 0x400000000
+
+void arch_sync_dma_for_device(phys_addr_t paddr, size_t size, enum dma_data_direction dir)
+{
+	sbi_cache_flush(paddr, size);
+}
+
+void arch_sync_dma_for_cpu(phys_addr_t paddr, size_t size, enum dma_data_direction dir)
+{
+	switch (dir) {
+	case DMA_BIDIRECTIONAL:
+		sbi_cache_flush(paddr, size);
+		break;
+	case DMA_FROM_DEVICE:
+		sbi_cache_invalidate(paddr, size);
+		break;
+	case DMA_TO_DEVICE:
+		break;
+	default:
+		BUG();
+	}
+}
+
+void arch_dma_prep_coherent(struct page *page, size_t size)
+{
+	void *flush_addr = page_address(page);
+
+	sbi_cache_flush(__pa(flush_addr), size);
+}
+
+void arch_dma_clear_uncached(void *addr, size_t size)
+{
+	memunmap(addr);
+}
+
+void *arch_dma_set_uncached(void *addr, size_t size)
+{
+	phys_addr_t phys_addr = __pa(addr) + DUBHE_UNCACHED_OFFSET;
+	void *mem_base = NULL;
+
+	mem_base = memremap(phys_addr, size, MEMREMAP_WT);
+	if (!mem_base) {
+		pr_err("%s memremap failed for addr %px\n", __func__, addr);
+		return ERR_PTR(-EINVAL);
+	}
+
+	return mem_base;
+}
+
+#else
 void arch_sync_dma_for_device(phys_addr_t paddr, size_t size,
 			      enum dma_data_direction dir)
 {
@@ -88,6 +142,7 @@ void arch_dma_prep_coherent(struct page *page, size_t size)
 
 	ALT_CMO_OP(flush, flush_addr, size, riscv_cbom_block_size);
 }
+#endif
 
 void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 		const struct iommu_ops *iommu, bool coherent)
