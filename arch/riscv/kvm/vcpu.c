0426313cbe7617a57424bde458964084da93979e
Ley Foon Tan <leyfoon.tan@starfivetech.com>
2023-08-07 14:21:34 +0800

    riscv: kvm: Fix compilation warning for unused kvm_riscv_vcpu_update_config()
    Commit 338a93e3aea9 ("arch: riscv: kvm: Temporarily comment out
    henvcfg-related code") has commented out calling to
    kvm_riscv_vcpu_update_config() function. So, comment out
    kvm_riscv_vcpu_update_config() function to resolve unused function
    warning.
    
    arch/riscv/kvm/vcpu.c:807:13: warning: 'kvm_riscv_vcpu_update_config' defined
    but not used [-Wunused-function]
      807 | static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
          |
    
    Fixes: 338a93e3aea9 ("arch: riscv: kvm: Temporarily comment out henvcfg-related code")
    Signed-off-by: Ley Foon Tan <leyfoon.tan@starfivetech.com>

--- arch/riscv/kvm/vcpu.c
+++ arch/riscv/kvm/vcpu.c
@@ -804,6 +804,7 @@ int kvm_arch_vcpu_ioctl_set_guest_debug(struct kvm_vcpu *vcpu,
 	return -EINVAL;
 }
 
+/*
 static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 {
 	u64 henvcfg = 0;
@@ -822,6 +823,7 @@ static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 	csr_write(CSR_HENVCFGH, henvcfg >> 32);
 #endif
 }
+*/
 
 void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 {
