621907abf08af35c8869bcce537e0e9040c3a40a
Thomas Gleixner <tglx@linutronix.de>
2023-09-26 13:03:52 +0000

    printk: nbcon: Add printer thread wakeups
    Add a function to wakeup the printer threads. Use the new function
    when:
    
      - records are added to the printk ringbuffer
      - consoles are resumed
      - triggered via printk_trigger_flush()
    
    The actual waking is performed via irq_work so that the wakeup can
    be triggered from any context.
    
    Co-developed-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Thomas Gleixner (Intel) <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2409,6 +2409,8 @@ asmlinkage int vprintk_emit(int facility, int level,
 		}
 	}
 
+	nbcon_wake_threads();
+
 	if (do_trylock_unlock) {
 		/*
 		 * The caller may be holding system-critical or
@@ -2711,6 +2713,10 @@ void resume_console(void)
 	 */
 	synchronize_srcu(&console_srcu);
 
+	/*
+	 * Since this runs in task context, wake the threaded printers
+	 * directly rather than scheduling irq_work to do it.
+	 */
 	cookie = console_srcu_read_lock();
 	for_each_console_srcu(con) {
 		flags = console_srcu_read_flags(con);
@@ -4118,6 +4124,7 @@ void defer_console_output(void)
 
 void printk_trigger_flush(void)
 {
+	nbcon_wake_threads();
 	defer_console_output();
 }
 
