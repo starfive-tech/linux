e729b64bb83be8fc0d03f81673278f2d78fa4748
Windsome Zeng <windsome.zeng@starfivetech.com>
2024-01-11 18:29:10 +0800

    riscv: cpu: cache: Implement a new method to flush the entire L2 cache
    According to the manual of SiFive U74, implement a new method to flush the entire L2 cache by using the Zero Device.
    
    After testing, 512KB is the critical point between the old and new way. It's better to use sifive_ccache_flush_entire
    function while data size is larger than cache size. Or it will improve more at 512KB when you know what you are doing.
    
    Signed-off-by: Windsome Zeng <windsome.zeng@starfivetech.com>

--- include/soc/sifive/sifive_ccache.h
+++ include/soc/sifive/sifive_ccache.h
@@ -27,6 +27,7 @@ static inline bool sifive_ccache_handle_noncoherent(void)
 #endif
 }
 
+void sifive_ccache_flush_entire(void);
 void sifive_ccache_flush_range(phys_addr_t start, size_t len);
 void sifive_l2_flush64_range(phys_addr_t start, size_t len);
 void *sifive_ccache_set_uncached(void *addr, size_t size);
