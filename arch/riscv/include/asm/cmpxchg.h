6375c0c6303dbb097117c2d1ec48d9af1c909282
Jisheng Zhang <jszhang@kernel.org>
2024-03-25 19:10:38 +0800

    riscv: cmpxchg: implement arch_cmpxchg64_{relaxed|acquire|release}
    [ Upstream commit 79d6e4eae9662b9103fecf94d52b44deca56743c ]
    
    After selecting ARCH_USE_CMPXCHG_LOCKREF, one straight futher
    optimization is implementing the arch_cmpxchg64_relaxed() because the
    lockref code does not need the cmpxchg to have barrier semantics. At
    the same time, implement arch_cmpxchg64_acquire and
    arch_cmpxchg64_release as well.
    
    However, on both TH1520 and JH7110 platforms, I didn't see obvious
    performance improvement with Linus' test case [1]. IMHO, this may
    be related with the fence and lr.d/sc.d hw implementations. In theory,
    lr/sc without fence could give performance improvement over lr/sc plus
    fence, so add the code here to leave performance improvement room on
    newer HW platforms.
    
    Link: http://marc.info/?l=linux-fsdevel&m=137782380714721&w=4 [1]
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>
    Reviewed-by: Andrea Parri <parri.andrea@gmail.com>
    Link: https://lore.kernel.org/r/20240325111038.1700-3-jszhang@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/include/asm/cmpxchg.h
+++ arch/riscv/include/asm/cmpxchg.h
@@ -205,4 +205,22 @@
 	arch_cmpxchg_relaxed((ptr), (o), (n));				\
 })
 
+#define arch_cmpxchg64_relaxed(ptr, o, n)				\
+({									\
+	BUILD_BUG_ON(sizeof(*(ptr)) != 8);				\
+	arch_cmpxchg_relaxed((ptr), (o), (n));				\
+})
+
+#define arch_cmpxchg64_acquire(ptr, o, n)				\
+({									\
+	BUILD_BUG_ON(sizeof(*(ptr)) != 8);				\
+	arch_cmpxchg_acquire((ptr), (o), (n));				\
+})
+
+#define arch_cmpxchg64_release(ptr, o, n)				\
+({									\
+	BUILD_BUG_ON(sizeof(*(ptr)) != 8);				\
+	arch_cmpxchg_release((ptr), (o), (n));				\
+})
+
 #endif /* _ASM_RISCV_CMPXCHG_H */
