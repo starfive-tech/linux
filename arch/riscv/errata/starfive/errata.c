7855f8095cd1c4839871f8c7c4a10ba961b7d175
Ji Sheng Teoh <jisheng.teoh@starfivetech.com>
2024-04-19 18:15:03 +0800

    riscv: errata: starfive: Remove CMO errata
    CMO non-standard ops is registered through cache driver and enabled
    through compatible string in device tree.
    
    Signed-off-by: Ji Sheng Teoh <jisheng.teoh@starfivetech.com>

--- arch/riscv/errata/starfive/errata.c
+++ arch/riscv/errata/starfive/errata.c
@@ -10,11 +10,9 @@
 #include <linux/module.h>
 
 #include <asm/alternative.h>
-#include <asm/cacheflush.h>
 #include <asm/errata_list.h>
 #include <asm/patch.h>
 #include <asm/processor.h>
-#include <asm/sbi.h>
 #include <asm/vendorid_list.h>
 
 #define STARFIVE_DUBHE90_MARCHID	0x80000000DB000090UL
@@ -27,26 +25,6 @@
 #define STARFIVE_DUBHE70_MIMPID		0x0000000020240131UL
 
 DEFINE_STATIC_KEY_FALSE(bypass_envcfg_csr_key);
-
-static void errata_probe_cmo(unsigned int stage, unsigned long arch_id,
-			     unsigned long impid)
-{
-	if (!IS_ENABLED(CONFIG_ERRATA_STARFIVE_CMO))
-		return;
-
-	if (arch_id == STARFIVE_DUBHE90_MARCHID) {
-		if (impid > STARFIVE_DUBHE90_MIMPID)
-			return;
-	}
-
-	if (arch_id == STARFIVE_DUBHE80_MARCHID) {
-		if (impid > STARFIVE_DUBHE80_MIMPID)
-			return;
-	}
-
-	riscv_cbom_block_size = L1_CACHE_BYTES;
-	riscv_noncoherent_supported();
-}
 
 static void errata_bypass_envcfg_csr(unsigned int stage, unsigned long arch_id,
 				     unsigned long impid)
@@ -74,7 +52,6 @@ void starfive_errata_patch_func(struct alt_entry *begin,
 		return;
 
 	if (stage == RISCV_ALTERNATIVES_BOOT) {
-		errata_probe_cmo(stage, archid, impid);
 		errata_bypass_envcfg_csr(stage, archid, impid);
 	}
 }
