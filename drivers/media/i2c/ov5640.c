c4b106e7f570222cf515acd3f44600eaceb700a8
Som Qin <som.qin@starfivetech.com>
2024-12-12 11:38:35 +0800

    vin: Add OV5640 camera sensor support for pinetab-v
    Signed-off-by: Som Qin <som.qin@starfivetech.com>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -276,6 +276,20 @@ static const struct ov5640_pixfmt ov5640_csi2_formats[] = {
 		.mux		= OV5640_FMT_MUX_YUV422,
 	}, {
 		/* YUV422, UYVY */
+		.code		= MEDIA_BUS_FMT_UYVY8_2X8,
+		.colorspace	= V4L2_COLORSPACE_SRGB,
+		.bpp		= 16,
+		.ctrl00		= 0x3f,
+		.mux		= OV5640_FMT_MUX_YUV422,
+	}, {
+		/* YUV422, YUYV */
+		.code		= MEDIA_BUS_FMT_YUYV8_2X8,
+		.colorspace	= V4L2_COLORSPACE_SRGB,
+		.bpp		= 16,
+		.ctrl00		= 0x30,
+		.mux		= OV5640_FMT_MUX_YUV422,
+	}, {
+		/* YUV422, UYVY */
 		.code		= MEDIA_BUS_FMT_UYVY8_1X16,
 		.colorspace	= V4L2_COLORSPACE_SRGB,
 		.bpp		= 16,
@@ -444,6 +458,7 @@ struct ov5640_dev {
 	struct regulator_bulk_data supplies[OV5640_NUM_SUPPLIES];
 	struct gpio_desc *reset_gpio;
 	struct gpio_desc *pwdn_gpio;
+	struct gpio_desc *sw_gpio;
 	bool   upside_down;
 
 	/* lock to protect all members below */
@@ -2481,6 +2496,9 @@ static int ov5640_set_power_on(struct ov5640_dev *sensor)
 	struct i2c_client *client = sensor->i2c_client;
 	int ret;
 
+	gpiod_set_value_cansleep(sensor->sw_gpio, 1);
+	// printk("ov5640_set_power_on sw = %d \n", gpiod_get_value(sensor->sw_gpio));
+
 	ret = clk_prepare_enable(sensor->xclk);
 	if (ret) {
 		dev_err(&client->dev, "%s: failed to enable clock\n",
@@ -2514,6 +2532,9 @@ xclk_off:
 
 static void ov5640_set_power_off(struct ov5640_dev *sensor)
 {
+	gpiod_set_value_cansleep(sensor->sw_gpio, 0);
+	// printk("ov5640_set_power_off sw = %d \n", gpiod_get_value(sensor->sw_gpio));
+
 	ov5640_power(sensor, false);
 	regulator_bulk_disable(OV5640_NUM_SUPPLIES, sensor->supplies);
 	clk_disable_unprepare(sensor->xclk);
@@ -3833,6 +3854,8 @@ static int ov5640_probe(struct i2c_client *client)
 	struct ov5640_dev *sensor;
 	int ret;
 
+	printk("ov5640_probe in linux/drivers/media/i2c/ov5640.c \n");
+
 	sensor = devm_kzalloc(dev, sizeof(*sensor), GFP_KERNEL);
 	if (!sensor)
 		return -ENOMEM;
@@ -3904,6 +3927,11 @@ static int ov5640_probe(struct i2c_client *client)
 						     GPIOD_OUT_HIGH);
 	if (IS_ERR(sensor->reset_gpio))
 		return PTR_ERR(sensor->reset_gpio);
+
+	sensor->sw_gpio = devm_gpiod_get_optional(dev, "sw",
+						     GPIOD_OUT_HIGH);
+	if (IS_ERR(sensor->sw_gpio))
+		return PTR_ERR(sensor->sw_gpio);
 
 	v4l2_i2c_subdev_init(&sensor->sd, client, &ov5640_subdev_ops);
 
