d58ef3ced434c224e55e163f064b4a06f9926c90
Ziv Xu <ziv.xu@starfivetech.com>
2025-08-11 17:26:38 +0800

    driver: net: wireless: aic8800D80: Reduce Wi-Fi startup logs
    Reduce Wi-Fi startup logs
    
    Signed-off-by: Ziv Xu <ziv.xu@starfivetech.com>

--- drivers/net/wireless/aic8800D80/aic8800D80_fdrv/rwnx_main.c
+++ drivers/net/wireless/aic8800D80/aic8800D80_fdrv/rwnx_main.c
@@ -512,7 +512,7 @@ static const int rwnx_hwq2uapsd[NL80211_NUM_ACS] = {
 
 
 extern uint8_t scanning;
-int aicwf_dbg_level = LOGERROR|LOGINFO|LOGDEBUG|LOGTRACE;
+int aicwf_dbg_level = 0;
 module_param(aicwf_dbg_level, int, 0660);
 int testmode = 0;
 char aic_fw_path[200];
